910845ba004f25e9e3bbcb540f88347e9d9e211b
Peter Huynh <peterhyh@yahoo.com>
2023-12-14 17:52:17 -0600

    title added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ window = tkinter.Tk()
 window.title("[TITLE HERE]")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Label testing", font=("Ariel", 24, "italic"))
+my_label = tkinter.Label(text="Title", font=("Ariel", 24, "italic"))
 my_label.pack()
 
 window.mainloop()
