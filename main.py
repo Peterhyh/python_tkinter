5998fe8839b3f00d9c1b409c247f96cfefc81222
Peter Huynh <peterhyh@yahoo.com>
2023-12-14 12:10:15 -0600

    tkinter label

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("[TITLE HERE]]")
+window.title("[TITLE HERE]")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="Label testing", font=("Ariel", 24, "italic"))
+my_label.pack()
 
 window.mainloop()
