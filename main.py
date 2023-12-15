78a05259a9135ab13756c6d3cfab4c34af037594
Peter Huynh <peterhyh@yahoo.com>
2023-12-15 17:20:48 -0600

    text input added

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ my_label.pack()
 
 
 def button_click():
-    my_label.config(text="Title Changed")
+    my_label.config(text=f"{entry.get()}")
 
 
 button = tkinter.Button(text="Click Me", command=button_click)
 button.pack()
+
+
+entry = tkinter.Entry(width=10)
+entry.pack()
+
 
 window.mainloop()
