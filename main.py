ee08c9d58e5ca8b15381127044431513e7e89c75
Peter Huynh <peterhyh@yahoo.com>
2023-12-15 12:00:34 -0600

    button added to change title name

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="Title", font=("Ariel", 24, "italic"))
 my_label.pack()
 
+
+def button_click():
+    my_label.config(text="Title Changed")
+
+
+button = tkinter.Button(text="Click Me", command=button_click)
+button.pack()
+
 window.mainloop()
