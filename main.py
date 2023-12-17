4f64ac42f4e5bca178bd9143adf9593b9e753b81
Peter Huynh <peterhyh@yahoo.com>
2023-12-17 11:35:10 -0600

    gui title changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("[TITLE HERE]")
+window.title("My Program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="Title", font=("Ariel", 24, "italic"))
