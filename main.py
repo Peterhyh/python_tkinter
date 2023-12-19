6b87f083ff530307423d7efac97f257905589565
Peter Huynh <peterhyh@yahoo.com>
2023-12-19 13:31:29 -0600

    tkinter grid added

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ window.title("My Program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="Title", font=("Ariel", 25, "italic"))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 
 def button_click():
     my_label.config(text=f"{entry.get()}")
 
 
-button = tkinter.Button(text="Click Me", command=button_click)
-button.pack()
+button = tkinter.Button(text="Button", command=button_click)
+button.grid(column=1, row=1)
+
+
+new_button = tkinter.Button(text="New Button")
+new_button.grid(column=2, row=0)
 
 
 entry = tkinter.Entry(width=10)
-entry.pack()
+entry.grid(column=3, row=2)
 
 
 window.mainloop()
